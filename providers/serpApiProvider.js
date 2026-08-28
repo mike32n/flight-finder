5c91e3c4fdc5b2aa4105c7e33e5299bb8ab5b539
Mihaly Horvath <mihaly32horvath@gmail.com>
2026-08-29 00:57:36 +0200

    fix missing bookingUrl

--- providers/serpApiProvider.js
+++ providers/serpApiProvider.js
@@ -55,7 +55,7 @@ class SerpApiProvider extends BaseProvider {
             return: returnDate,
             price: Number(cheapest.price),
             currency: "HUF",
-            bookingUrl: response.data.search_metadata.google_flights_url,
+            bookingUrl: response.data?.search_metadata?.google_flights_url,
           },
         };
       } catch (error) {
