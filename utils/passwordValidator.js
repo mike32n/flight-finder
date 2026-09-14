4cc121457d4bf7b68c8125e82f4e21608f1315ea
Mihaly Horvath <mihaly32horvath@gmail.com>
2026-09-14 15:57:36 +0200

    password validator helper

---
+++ utils/passwordValidator.js
@@ -0,0 +1,23 @@
+function validatePassword(password) {
+  if (!password) {
+    return "Password is required.";
+  }
+
+  if (password.length < 8) {
+    return "Password must be at least 8 characters.";
+  }
+
+  const hasUppercase = /[A-Z]/.test(password);
+  const hasLowercase = /[a-z]/.test(password);
+  const hasNumber = /\d/.test(password);
+
+  if (!hasUppercase || !hasLowercase || !hasNumber) {
+    return "Password must contain uppercase, lowercase and number.";
+  }
+
+  return null;
+}
+
+module.exports = {
+  validatePassword,
+};
